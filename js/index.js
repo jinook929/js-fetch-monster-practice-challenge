5bbfbb7407896558305d915ad677c713a5057d0c
jinook929 <jinook929@gmail.com>
2021-04-26 19:47:37 -0400

    commit tree dd8cb397a71c31b5bf8f8e34273284d3ac8a616b

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-// Q1. why refreshing?
+// Do not use live server not to reload the page after adding a new monster!!! Just open the index.html file with the browser.
 
 let page = 1 // current page default setting
 let totalPages = 0 // total page default setting
 const url = "http://localhost:3000/monsters" // server main url
 
-const postMonster = function(submit) {
-  // set event listener on submit
-  submit.addEventListener('click', (e) => {
+const postMonster = function() {
+  // set event listener to form on submit (this === form)
+  this.addEventListener('submit', (e) => {
     e.preventDefault()
     console.dir(this[0].value)
     console.dir(Number.parseInt(this[1].value))
@@ -25,7 +25,12 @@ const postMonster = function(submit) {
         description: this[2].value
       })
     }).then(res => res.json())
-    .then(data => {console.log(data)})
+    .then(data => {
+      console.log(data)
+      this.reset()
+      newPage()
+    })
+    .catch(e => console.log("Error:", e))
   })
 }
 
@@ -48,7 +53,7 @@ const createForm = () => {
   // create input & placeholer for name
   const name = document.createElement("input")
   name.id = "name"
-  name.placeholder = "Monster Name"
+  name.placeholder = "New Monster Name"
 
   // create input & placeholer for age
   const age = document.createElement("input")
@@ -78,7 +83,7 @@ const createForm = () => {
   pageNum.forEach(el => el.innerHTML = page)
 
   // post info to database
-  postMonster.call(form, submit)
+  postMonster.call(form)
 }
 
 const makeMosterCards = (monsters) => {
@@ -87,11 +92,11 @@ const makeMosterCards = (monsters) => {
   monsters.forEach((monster, i) => {
     // get monster info
     const name = monster.name
-    const age = monster.age
+    const age = Number.parseInt(monster.age)
     const description = monster.description
     const id = monster.id
 
-    // form card
+    // create card
     const card = document.createElement("div")
     card.className = "card"
     if(i === 0) card.classList.add("active")
@@ -108,18 +113,19 @@ const makeMosterCards = (monsters) => {
 
     // show in the main display (#monster-container)
     monsterContainer.append(card)
-    return monsterContainer
+    // return monsterContainer
   })
 }
 
 const getMonsters = function() {
-  fetch(`${url}?_sort=id&_order=desc&_page=${page}&_limit=10`)
+  const monstersPerPage = 20
+  fetch(`${url}?_sort=id&_order=desc&_page=${page}&_limit=${monstersPerPage}`)
   .then(res => res.json())
   .then(monsters => {
     console.log("Fetched Monsters =>", monsters)
     async function totalMonsters(url) {
       const totalNum = await fetch(url).then(res => res.json()).then(total => total.length)
-      totalPages = Math.ceil(totalNum / 10)
+      totalPages = Math.ceil(totalNum / monstersPerPage)
       makeMosterCards(monsters)
     }
     totalMonsters(url)
@@ -149,7 +155,6 @@ const navigatePages = () => {
 
   next.forEach( nx => {
     nx.addEventListener('click', function(e) {
-      console.log(this)
       if(page !== totalPages) {
         page++
         newPage()
